56e9b2cb8180cf0776b46d8cd9f1fd672536c488
revoltek <rev@voo.it>
2013-09-29 22:11:03 +0200

    flux2lum

---
+++ flux2lum.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# flux2lum.py flux (in Jy)
+
+import sys, os
+import numpy as np
+
+flux = float(sys.argv[1]) # in Jy
+dist = 45.6 # in Mpc
+
+# 1 pc = 3.08567758e16 m
+print flux * 1e-26 * ( 4*np.pi * (dist * 1e6 * 3.08567758e16)**2),
+print "W/hz"
+print "(For a distance of "+str(dist)+")"
