91e7623a3281c9c6c1badd3d14546f71331fa86e
revoltek <rev@voo.it>
2015-04-27 10:18:19 +0000

    new algorithm for mask making

--- make_mask.py
+++ make_mask.py
@@ -2,61 +2,38 @@
 
 # create a mask using bdsm of an image
 
-def make_mask(image_name, threshpix=5, threshisl=3, atrous_do=False, mask_name=None, rmsbox=(120,40), mask_combine=None):
+def make_mask(image_name, threshpix=5, threshisl=3, atrous_do=False, mask_name=None, rmsbox=(55,12), mask_combine=None):
 
     import sys, os, numpy
     import pyfits, pyrap
     import lofar.bdsm as bdsm
 
+    # wavelets are required to fit gaussians
+    if atrous_do: stop_at = None
+    else: stop_at = 'isl'
+
     # DO THE SOURCE DETECTION
     print "Running source detector"
-    img = bdsm.process_image( image_name, mean_map='zero', adaptive_rms_box=True, rms_box_bright=(20, 7), rms_box=rmsbox,\
-        thresh_pix=int(threshpix), thresh_isl=int(threshisl), atrous_do=atrous_do, ini_method='curvature', advanced_opts=True, blank_limit=1e-5)
+    img = bdsm.process_image(image_name, mean_map='zero', rms_box=rmsbox, \
+        thresh_pix=int(threshpix), thresh_isl=int(threshisl), atrous_do=atrous_do, atrous_jmax=3, \
+        adaptive_rms_box=True, adaptive_thresh=150, rms_box_bright=(80,20), \
+        stop_at=stop_at, blank_limit=1e-5)
 
-    # DEBUG
-    #soumodel = image_name.replace('.image','.skymodel')
-    #if os.path.exists(soumodel): os.system('rm ' + soumodel)
-    #img.export_image(img_type='sou_model', outfile=soumodel)
-
-    # WRITE THE GAUSSIAN MODEL FITS
-    gausmodel = image_name.replace('.image','.gausmodel').replace('.restored.corr','.gausmodel').replace('.fits','.gausmodel')
-    assert gausmodel != image_name # prevent overwriting
-    if os.path.exists(gausmodel): os.system('rm ' + gausmodel)
-    img.export_image(img_type='gaus_model', outfile=gausmodel)
-
-    hdulist = pyfits.open(gausmodel)
-    pixels_gs = hdulist[0].data
-
-    # convert to casa-image if fits file
-    if image_name[-4:] == 'fits':
-        print "Converting to fits file..."
-        img = pyrap.images.image(image_name)
-        image_name = image_name.replace('.fits','.image')
-        img.saveas(image_name, overwrite=True)
-        del img
-
-    if mask_name == None: mask_name = image_name.replace('.image','.mask').replace('.restored.corr','.mask')
-    print 'Making mask:', mask_name
+    # WRITE THE MASK FITS
+    if mask_name == None: mask_name = image_name+'.newmask'
     if os.path.exists(mask_name): os.system('rm -r ' + mask_name)
-    os.system('cp -r ' + image_name + ' ' + mask_name)
+    print 'Making mask:', mask_name
+    img.export_image(img_type='island_mask', img_format='casa', outfile=mask_name)
 
-    img = pyrap.images.image(mask_name)
-    pixels = numpy.copy(img.getdata())
-    pixels_mask = 0. * numpy.copy(pixels)
-
-    gs_cut = 1e-5
-    idx = numpy.where(pixels_gs > gs_cut)
-    pixels_mask[idx] = 1.
-    
     # do an pixel-by-pixel "OR" operation with a given mask
     if mask_combine != None:
         print "Combining with "+mask_combine
+        img = pyrap.images.image(mask_name)
+        pixels_mask = img.getdata()
         imgcomb = pyrap.images.image(mask_combine)
         assert imgcomb.shape() == img.shape()
-        pixels_mask[numpy.where(imgcomb.getdata() == 1.)] = 1.
-
-    img.putdata(pixels_mask)
-    del img
+        pixels_mask[np.where(imgcomb.getdata() == 1.)] = 1.
+        img.putdata(pixels_mask)
 
     return mask_name
 
@@ -68,7 +45,7 @@ if __name__=='__main__':
     opt.add_option('-t', '--atrous_do', help='BDSM extended source detection (default=False)', action='store_true', default=False)
     opt.add_option('-m', '--newmask', help='Mask name (default=imagename with mask in place of image)', default=None)
     opt.add_option('-c', '--combinemask', help='Mask name of a mask to add to the found one (default=None)', default=None)
-    opt.add_option('-r', '--rmsbox', help='rms box size (default=120,40)', default='120,40')
+    opt.add_option('-r', '--rmsbox', help='rms box size (default=55,12)', default='55,12')
     (options, args) = opt.parse_args()
     
     rmsbox = (int(options.rmsbox.split(',')[0]),int(options.rmsbox.split(',')[1]))
