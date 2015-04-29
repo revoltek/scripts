196e446604598d33f0da7222ef91b6057a0aac3a
revoltek <rev@voo.it>
2015-04-29 15:02:57 +0000

    casa_comm rep

---
+++ autocal/casa_comm/1RXSJ0603_LBA/casa_clean_peel.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# casapy --nogui --log2term --nologger -c this_script.py picklefile
+
+import os, sys, pickle
+
+def casa_clean(msfile='', imagename='', imsize=1024, mask='', niter=1000, multiscale=[], wproj=512):
+    default('clean')
+    clean(vis=msfile,imagename=imagename,mode="mfs",gridmode="widefield",wprojplanes=wproj,niter=niter,gain=0.1,psfmode="clark",\
+        imagermode="csclean",multiscale=multiscale,interactive=False, mask=mask,imsize=[int(imsize)],cell=['3arcsec'],weighting="briggs",robust=0,\
+        usescratch=False,cyclefactor=5,cyclespeedup=-1,nterms=2)
+
+params = pickle.load( open( sys.argv[6], "rb" ) )
+os.system('rm '+sys.argv[6])
+casa_clean(**params)
---
+++ autocal/casa_comm/casa_blank.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# casapy --nogui --log2term --nologger -c  casa_blank.py picklefile
+# blank all the pixel outside this region
+# NOTE: overwrite the image! And the blanked image is the smallest around the given region.
+
+import os, sys, pickle
+
+def casa_blank(imgs = [], region = '', inverse=False, setTo = 0):
+    """
+    img = image to blank
+    region = blank all pixels inside this region to the value "setTo"
+    setTo = value to set pixels
+    inverse = instead of blanking pixel return an image that covers only the "region" region
+    out: the new, blanked image will overwrite the old one
+    """
+    if imgs is str: imgs = [imgs]
+    for img in imgs:
+        if inverse:
+            default('immath')
+            immath(imagename = img, mode = 'evalexpr', expr = 'IM0', region = region, outfile = img+'-out')
+            os.system('rm -r '+img)
+            os.system('mv '+img+'-out '+img)
+        else:
+            ia.open(img)
+            reg = rg.fromtextfile(filename=region, shape=ia.shape(), csys=ia.coordsys().torecord())
+            ia.set(pixels=setTo, region=reg)
+            ia.close()
+
+params = pickle.load( open( sys.argv[6], "rb" ) )
+os.system('rm '+sys.argv[6])
+casa_blank(**params)
