6d79ff7fa175e81c397570483f6e0def3df791d5
revoltek <rev@voo.it>
2015-05-28 16:16:09 +0000

    autocal update

--- autocal/VirA_LBA/runTAMMOenv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# run a commmand in CITT environment
-source /home/dijkema/opt/lofar/LOFAR-DP3predict-Task6970/lofarinit.sh
-$@
--- autocal/casa_comm/virgoLBAis/casa_clean.py
+++ autocal/casa_comm/virgoLBAis/casa_clean.py
@@ -3,7 +3,7 @@
 
 import sys, pickle
 
-def casa_clean(msfile='', imagename='', imtype='normal'):
+def casa_clean(msfile='', imagename='', imtype='normal', uvrange=''):
     """
     imtype: 'lr' for low resolution, large version of the image (larger FoV to get possible new extended emission)
             'wide' for wide FoV image
@@ -12,26 +12,27 @@ def casa_clean(msfile='', imagename='', imtype='normal'):
 
     if imtype == 'normal': 
         default('clean')
-        clean(vis=msfile,imagename=imagename,mode="mfs",gridmode="widefield",wprojplanes=512,niter=300,gain=0.05,psfmode="clark",imagermode="csclean",multiscale=[],interactive=False,mask='/home/fdg/scripts/autocal/VirA_LBA/m87.crtf',imsize=[2500],cell=['0.5arcsec'],weighting="briggs",robust=0,cyclefactor=8,cyclespeedup=-1,nterms=3)
+        clean(vis=msfile,imagename=imagename,mode="mfs",gridmode="widefield",wprojplanes=512,niter=300,gain=0.05,psfmode="clark",imagermode="csclean",multiscale=[],interactive=False,mask='/home/fdg/scripts/autocal/VirA_LBA/m87.crtf',imsize=[2500],cell=['0.5arcsec'],weighting="briggs",robust=0,cyclefactor=8,cyclespeedup=-1,nterms=3,uvrange=uvrange)
 
         default('clean')
-        scales=[0, 5, 10, 20, 40, 80, 160, 320]
-        clean(vis=msfile,imagename=imagename,mode="mfs",gridmode="widefield",wprojplanes=512,niter=10000,gain=0.1,psfmode="clark",imagermode="csclean",multiscale=scales,interactive=False,mask='/home/fdg/scripts/autocal/VirA_LBA/m87.crtf',imsize=[2500],cell=['0.5arcsec'],weighting="briggs",robust=0,cyclefactor=5,cyclespeedup=-1,nterms=3)
+        scales=[0, 5, 20, 80, 320]
+        clean(vis=msfile,imagename=imagename,mode="mfs",gridmode="widefield",wprojplanes=512,niter=10000,gain=0.1,psfmode="clark",imagermode="csclean",multiscale=scales,interactive=False,mask='/home/fdg/scripts/autocal/VirA_LBA/m87.crtf',imsize=[2500],cell=['0.5arcsec'],weighting="briggs",robust=0,cyclefactor=5,cyclespeedup=-1,nterms=3,uvrange=uvrange)
 
     elif imtype == 'lr':
         default('clean')
-        clean(vis=msfile,imagename=imagename,mode="mfs",gridmode="widefield",wprojplanes=1024,niter=50,gain=0.05,psfmode="clark",imagermode="csclean",multiscale=[],interactive=False,mask='/home/fdg/scripts/autocal/VirA_LBA/m87.crtf',imsize=[512],cell=['10arcsec'],weighting="briggs",robust=0,cyclefactor=8,cyclespeedup=-1,nterms=3,uvtaper=T,outertaper='60arcsec')
+        clean(vis=msfile,imagename=imagename,mode="mfs",gridmode="widefield",wprojplanes=1024,niter=50,gain=0.05,psfmode="clark",imagermode="csclean",multiscale=[],interactive=False,mask='/home/fdg/scripts/autocal/VirA_LBA/m87.crtf',imsize=[512],cell=['10arcsec'],weighting="briggs",robust=0,cyclefactor=8,cyclespeedup=-1,nterms=3,uvtaper=T,outertaper='60arcsec',uvrange=uvrange)
 
         default('clean')
         scales=[0, 5, 10, 20, 40]
-        clean(vis=msfile,imagename=imagename,mode="mfs",gridmode="widefield",wprojplanes=1024,niter=5000,gain=0.1,psfmode="clark",imagermode="csclean",multiscale=scales,interactive=False,mask='/home/fdg/scripts/autocal/VirA_LBA/m87.crtf',imsize=[512],cell=['10arcsec'],weighting="briggs",robust=0,cyclefactor=5,cyclespeedup=-1,nterms=3,uvtaper=T,outertaper='60arcsec')
+        clean(vis=msfile,imagename=imagename,mode="mfs",gridmode="widefield",wprojplanes=1024,niter=5000,gain=0.1,psfmode="clark",imagermode="csclean",multiscale=scales,interactive=False,mask='/home/fdg/scripts/autocal/VirA_LBA/m87.crtf',imsize=[512],cell=['10arcsec'],weighting="briggs",robust=0,cyclefactor=5,cyclespeedup=-1,nterms=3,uvtaper=T,outertaper='60arcsec',uvrange=uvrange)
 
     elif imtype == 'wide':
         default('clean')
-        clean(vis=msfile,imagename=imagename,mode="mfs",gridmode="widefield",wprojplanes=1024,niter=500,gain=0.05,psfmode="clark",imagermode="csclean",multiscale=[],interactive=False,imsize=[1024],cell=['10arcsec'],weighting="briggs",robust=0,cyclefactor=5,cyclespeedup=-1,nterms=3,uvtaper=T,outertaper='120arcsec')
+        clean(vis=msfile,imagename=imagename,mode="mfs",gridmode="widefield",wprojplanes=1024,niter=500,gain=0.05,psfmode="clark",imagermode="csclean",multiscale=[],interactive=False,imsize=[1024],cell=['10arcsec'],weighting="briggs",robust=0,cyclefactor=5,cyclespeedup=-1,nterms=3,uvtaper=T,outertaper='120arcsec',uvrange=uvrange)
 
     else:
-        print "Wrong imtype."
+        print "Error: Wrong imtype."
+        return 1
 
 params = pickle.load( open( sys.argv[6], "rb" ) )
 os.system('rm '+sys.argv[6])
