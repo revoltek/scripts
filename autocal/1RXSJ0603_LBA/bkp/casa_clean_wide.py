89b191e9735a14ef589e8a6c107c38179aeddf24
revoltek <rev@voo.it>
2015-07-28 15:12:18 +0200

    useless bkp removed

--- autocal/1RXSJ0603_LBA/bkp/casa_clean_wide.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# casapy --nogui --log2term --nologger -c this_script.py
-
-import os, sys
-
-args = sys.argv[6:]
-
-active_ms = args[0]
-clean(vis=active_ms,imagename="img/"+active_ms.replace('.MS', '-wide'),mode="mfs",gridmode="widefield",wprojplanes=512,niter=5000,gain=0.1,psfmode="clark",imagermode="csclean",multiscale=[0,3,9,27],interactive=False,mask='/cep3home/fdg/scripts/autocal/1RXSJ0603_LBA/tooth_mask.crtf',imsize=[4096],cell=['5arcsec'],weighting="briggs",robust=0,cyclefactor=5,cyclespeedup=-1,nterms=2)
--- autocal/VirA_LBA/bkp/casa_split.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# MPIP: casaargs = msfile
-import sys
-
-args = sys.argv[6:]
-msfile = args[0]
-
-split(vis=msfile, outputvis=msfile.replace('.MS','.split.MS'))
