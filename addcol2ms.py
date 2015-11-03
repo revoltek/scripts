72ae31f8f286a4cb567ba51529f21b4f6045a387
revoltek <rev@voo.it>
2015-11-03 18:15:15 +0100

    multiple col

--- addcol2ms.py
+++ addcol2ms.py
@@ -14,23 +14,24 @@ def main(options):
     if inms == '':
             print 'Error: you have to specify an input MS, use -h for help'
             return
-    outcol = options.outcol
+    outcols = options.outcols
     
     t = pt.table(inms, readonly=False, ack=True)
 
-    if outcol not in t.colnames():
-        print 'Adding the output column',outcol,'to',inms
-        coldmi = t.getdminfo('DATA')
-        coldmi['NAME'] = outcol
-        t.addcols(pt.maketabdesc(pt.makearrcoldesc(outcol, 0., valuetype='complex', shape=numpy.array(t.getcell('DATA',0)).shape)), coldmi)  
-        data = t.getcol('DATA')
-        t.putcol(outcol, data)
-    else:
-      print outcol, 'column already exist'
+    for outcol in outcols.split(','):
+        if outcol not in t.colnames():
+            print 'Adding the output column',outcol,'to',inms,'.'
+            coldmi = t.getdminfo('DATA')
+            coldmi['NAME'] = outcol
+            t.addcols(pt.maketabdesc(pt.makearrcoldesc(outcol, 0., valuetype='complex', shape=numpy.array(t.getcell('DATA',0)).shape)), coldmi)  
+            data = t.getcol('DATA')
+            t.putcol(outcol, data)
+        else:
+            print outcol, 'Column'+outcol+' already exists.'
         
 opt = optparse.OptionParser()
-opt.add_option('-i','--inms',help='Input MS [no default]',default='')
-opt.add_option('-o','--outcol',help='Output column [no default]',default='')
+opt.add_option('-i','--inms',help='Input MS [no default].',default='')
+opt.add_option('-o','--outcols',help='Output column, comma separated if more than one [no default].',default='')
 options, arguments = opt.parse_args()
 main(options)
 
