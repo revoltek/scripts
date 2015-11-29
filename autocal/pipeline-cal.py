0173a6ef69563be36910fc8cae23c64d82482dd8
revoltek <rev@voo.it>
2015-11-29 14:32:29 +0100

    only_clock option

--- autocal/pipeline-cal.py
+++ autocal/pipeline-cal.py
@@ -12,6 +12,8 @@ parset_dir = '/home/fdg/scripts/autocal/VirA_LBA/parset_cal' # virgo LBA
 #skymodel = '/home/fdg/scripts/model/3C295-allfield.skymodel' # mode-test LBA
 #parset_dir = '/home/fdg/scripts/autocal/LBAmode/parset_cal' # mode-test LBA
 
+only_clock = True
+
 ###################################################
 
 import sys, os, glob, re
@@ -45,10 +47,11 @@ s.run(check=False)
 
 ############################################
 # TODO: If only clock is tarnsferred we need to oreoare a parmdb
-logging.info('Calibrating with skymodel: '+skymodel)
-for ms in mss:
-    s.add('calibrate-stand-alone -f --parmd-db instrument_fake '+ms+' '+parset_dir+'/bbs-fakeparmdb.parset '+skymodel, log=ms+'_fakeparmdb.log', cmd_type='BBS')
-s.run(check=True)
+if only_clock:
+    logging.info('Calibrating with skymodel: '+skymodel)
+    for ms in mss:
+        s.add('calibrate-stand-alone -f --parmd-db instrument_fake '+ms+' '+parset_dir+'/bbs-fakeparmdb.parset '+skymodel, log=ms+'_fakeparmdb.log', cmd_type='BBS')
+    s.run(check=True)
 
 ##############################################
 # Initial calibrator
@@ -61,30 +64,34 @@ s.run(check=True)
 ##############################################
 # Clock/TEC check and flagging
 check_rm('globaldb')
-os.system('mkdir globaldb-in')
-os.system('mkdir globaldb-out')
+os.system('mkdir globaldb')
+if only_clock: os.system('mkdir globaldb-clockonly')
 
 logging.info('Running LoSoTo...')
 for i, ms in enumerate(mss):
     num = re.findall(r'\d+', ms)[-1]
 
-    logging.debug('Copy instrument of '+ms+' into globaldb-in/instrument-'+str(num))
-    os.system('cp -r '+ms+'/instrument globaldb-in/instrument-'+str(num))
-    if i == 0: os.system('cp -r '+ms+'/ANTENNA '+ms+'/FIELD '+ms+'/sky globaldb-in/')
-
+    logging.debug('Copy instrument of '+ms+' into globaldb/instrument-'+str(num))
+    os.system('cp -r '+ms+'/instrument globaldb/instrument-'+str(num))
+    if i == 0: os.system('cp -r '+ms+'/ANTENNA '+ms+'/FIELD '+ms+'/sky globaldb/')
+    
     # TODO: If we export clock create a new parmdb
-    logging.debug('Copy instrument_fake of '+ms+' into globaldb-out/instrument-'+str(num))
-    os.system('cp -r '+ms+'/instrument_fake globaldb-out/instrument-'+str(num))
-    if i == 0: os.system('cp -r '+ms+'/ANTENNA '+ms+'/FIELD '+ms+'/sky globaldb-out/')
+    if only_clock:
+        logging.debug('Copy instrument_fake of '+ms+' into globaldb-clockonly/instrument-'+str(num))
+        os.system('cp -r '+ms+'/instrument_fake globaldb-clockonly/instrument-'+str(num))
+        if i == 0: os.system('cp -r '+ms+'/ANTENNA '+ms+'/FIELD '+ms+'/sky globaldb-clockonly/')
 
 check_rm('plots')
 os.makedirs('plots')
 check_rm('cal.h5')
-s.add('H5parm_importer.py -v cal.h5 globaldb-in', log='losoto.log', cmd_type='python')
+s.add('H5parm_importer.py -v cal.h5 globaldb', log='losoto.log', cmd_type='python')
 s.run(check=False)
 s.add('losoto -v cal.h5 '+parset_dir+'/losoto.parset', log='losoto.log', log_append=True, cmd_type='python', processors='max')
 s.run(check=False)
-s.add('H5parm_exporter.py -v cal.h5 globaldb-out', log='losoto.log', log_append=True, cmd_type='python')
+if only_clock:
+    s.add('H5parm_exporter.py -v cal.h5 globaldb-clockonly', log='losoto.log', log_append=True, cmd_type='python')
+else:
+    s.add('H5parm_exporter.py -v cal.h5 globaldb', log='losoto.log', log_append=True, cmd_type='python')
 s.run(check=True)
 
 logging.info("Done.")
