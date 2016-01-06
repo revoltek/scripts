9260f46764f25448035fdf06057bad1fa60d0a42
revoltek <rev@voo.it>
2016-01-06 15:39:34 +0000

    skip downloaded files

--- autocal/pipeline-download.py
+++ autocal/pipeline-download.py
@@ -5,7 +5,7 @@ download_file = 'cyg1.txt'
 
 ###################################################
 
-import sys, os, glob
+import sys, os, re, glob
 import numpy as np
 from lib_pipeline import *
 
@@ -16,9 +16,12 @@ s = Scheduler(dry=False, max_threads = 4, qsub=False) # set here max number of t
 df = open(download_file,'r')
 
 logging.info('Downloading...')
+downloaded = glob.glob('*MS')
 for i, line in enumerate(df):
-    s.add('wget -nv "'+line[:-1]+'" -O - | tar -x', log=str(i)+'.log', cmd_type='general')
-#    print 'wget -nv "'+line[:-1]+'" -O - | tar -x'
+    ms = re.findall(r'L[0-9]*_SB[0-9]*_uv\.dppp\.MS', line)[0]
+    if ms in downloaded: continue
+#    s.add('wget -nv "'+line[:-1]+'" -O - | tar -x', log=str(i)+'.log', cmd_type='general')
+    print 'wget -nv "'+line[:-1]+'" -O - | tar -x'
 s.run(check=True)
 
 logging.info("Done.")
