5605e5068b5bb52d1b76dccd8587dff14690548e
revoltek <rev@voo.it>
2017-02-08 23:58:42 +0000

    fix bugs

--- applycal.py
+++ applycal.py
@@ -9,9 +9,9 @@ import casacore.tables as pt
 import numpy as np
 
 direction = 0
-print "Applying dir %i", direction
+print "Applying dir %i" % direction
 
-t = pt.table(sys.arv[1], readonly=False)
+t = pt.table(sys.argv[1], readonly=False)
 h5 = tables.open_file(sys.argv[2])
 soltab_tec = h5.root.sol000.tec000
 soltab_csp = h5.root.sol000.scalarphase000
@@ -23,30 +23,31 @@ wgts_csp = soltab_csp.weight
 
 times = soltab_tec.time
 
-freqs = pt.table("TC00-first5.MS/SPECTRAL_WINDOW",ack=False)[0]["CHAN_FREQ"]
+freqs = pt.table(sys.argv[1]+"/SPECTRAL_WINDOW",ack=False)[0]["CHAN_FREQ"]
 
 for timestep, buf in enumerate(t.iter("TIME")):
     if timestep % 10 == 0: print "Timestep", timestep
 
-    assert buf.getcell("TIME",0) == times[timestep]
+    #print buf.getcell("TIME",0), times[timestep]
+    #assert buf.getcell("TIME",0) == times[timestep]
 
-    if (soltab.time[timestep]-buf[0]["TIME"]+0.5*buf[0]["INTERVAL"])>3:
+    if (soltab_tec.time[timestep]-buf[0]["TIME"]+0.5*buf[0]["INTERVAL"])>3:
       raise Exception("Error: times should match")
 
-    data = buf.getcol("DATA")
+    data = buf.getcol("CORRECTED_DATA")
     flag = buf.getcol("FLAG")
 
     for rownr, row in enumerate(buf):
         ant1 = row["ANTENNA1"]
         ant2 = row["ANTENNA2"]
-        if wgts_tec[ant1,direction,0,timestep] == 0 or wgts_tec[ant2,direction,0,timestep] == 0 or \
-           wgts_csp[ant1,direction,0,timestep] == 0 or wgts_csp[ant2,direction,0,timestep] == 0:
+        if wgts_tec[direction,ant1,0,timestep] == 0 or wgts_tec[direction,ant2,0,timestep] == 0 or \
+           wgts_csp[direction,ant1,0,timestep] == 0 or wgts_csp[direction,ant2,0,timestep] == 0:
             flag[rownr,channel,:] = True
             continue
 
-        g1 = sols_csp[ant1,direction,0,timestep] + sols_tec[ant1,direction,0,timestep] * 8.44797245e9 / freqs
+        g1 = sols_csp[direction,ant1,0,timestep] + sols_tec[direction,ant1,0,timestep] * 8.44797245e9 / freqs
         g1 = cos(g1) + 1j*sin(g1)
-        g2 = sols_csp[ant2,direction,0,timestep] + sols_tec[ant2,direction,0,timestep] * 8.44797245e9 / freqs
+        g2 = sols_csp[direction,ant2,0,timestep] + sols_tec[direction,ant2,0,timestep] * 8.44797245e9 / freqs
         g2 = cos(g2) + 1j*sin(g2)
         for pol in range(4):
             data[rownr,:,pol] *= ( g1 * np.conj(g2) )
