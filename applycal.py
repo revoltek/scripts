1193a210ab0d97710180959b139ada7bd2495181
revoltek <rev@voo.it>
2017-07-06 09:43:23 +0000

    update

--- applycal.py
+++ applycal.py
@@ -12,15 +12,16 @@ opt.add_option('-i','--inms',help='Input MS',default='')
 opt.add_option('--incol',help='Input column',default='DATA')
 opt.add_option('--outcol',help='Output column',default='CORRECTED_DATA')
 opt.add_option('--inh5',help='Input H5parm',default='')
-opt.add_option('-d','--dir',help='Direction (int)',type=int,default=0)
+opt.add_option('-d','--dir',help='Direction (string)',default=None)
 opt.add_option('-c','--corrupt',action="store_true",default=False,help='Corrupt')
 o, args = opt.parse_args()
 
 t = pt.table(o.inms, readonly=False)
 h5 = tables.open_file(o.inh5)
-direction = o.dir
 soltab_tec = h5.root.sol000.tec000
 soltab_csp = h5.root.sol000.scalarphase000
+directions = h5.root.sol000.source
+direction = np.argwhere(directions[:]['name'] == o.dir)[0][0]
 
 print "Applying dir %s" % h5.root.sol000.tec000.dir[direction]
 #print "CSP HAVE A MINUS TO COMPENSATE NDPPP BUG"
@@ -50,10 +51,10 @@ for timestep, buf in enumerate(t.iter("TIME")):
             flag[rownr,:,:] = True
             continue
 
-        g1 = sols_csp[ant1,direction,timestep] - sols_tec[ant1,direction,timestep] * 8.44797245e9 / freqs
+        g1 = sols_csp[ant1,direction,timestep] + sols_tec[ant1,direction,timestep] * 8.44797245e9 / freqs
         #g1 = -1. * sols_tec[ant1,direction,timestep] * 8.44797245e9 / freqs
         g1 = cos(g1) + 1j*sin(g1)
-        g2 = sols_csp[ant2,direction,timestep] - sols_tec[ant2,direction,timestep] * 8.44797245e9 / freqs
+        g2 = sols_csp[ant2,direction,timestep] + sols_tec[ant2,direction,timestep] * 8.44797245e9 / freqs
         #g2 = -1. * sols_tec[ant2,direction,timestep] * 8.44797245e9 / freqs
         g2 = cos(g2) + 1j*sin(g2)
         for pol in range(4):
