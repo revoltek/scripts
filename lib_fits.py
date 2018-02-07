374aa6c0998ee83c73b337dfa2285304e406287f
revoltek <rev@voo.it>
2018-02-07 17:51:53 +0100

    update

--- lib_fits.py
+++ lib_fits.py
@@ -26,6 +26,8 @@ def flatten(filename, channel=0, freqaxis=0):
 
     header = wn.to_header()
     header["NAXIS"]=2
+    header["NAXIS1"]=f[0].header['NAXIS1'] # Test
+    header["NAXIS2"]=f[0].header['NAXIS2'] # Test
     copy=('EQUINOX','EPOCH')
     for k in copy:
         r=f[0].header.get(k)
