fed71f1194f3806f1c54b6534be337606d05ca66
revoltek <rev@voo.it>
2018-03-14 15:45:11 +0100

    update

--- pybdsm_script.py
+++ pybdsm_script.py
@@ -5,9 +5,10 @@ import sys
 
 file = sys.argv[1]
 
-img = bdsf.process_image(file, advanced_opts=True, blank_zeros=True, interactive=True, thresh_pix=6., thresh_isl=4.5)
+img = bdsf.process_image(file, advanced_opts=True, interactive=True, thresh_pix=6., thresh_isl=4.5)
 img.write_catalog(format='fits',catalog_type='srl', clobber=True)
 img.write_catalog(format='ds9',catalog_type='gaul', clobber=True)
+img.write_catalog(format='bbs',catalog_type='gaul', clobber=True)
 img.export_image(outfile=file+'_gaus_resid.fits', img_type='gaus_resid',clobber=True)
 img.export_image(outfile=file+'_gaus_model.fits', img_type='gaus_model',clobber=True)
 img.export_image(outfile=file+'_rms.fits', img_type='rms',clobber=True)
