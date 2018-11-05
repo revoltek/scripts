c89dcab8f906a67420bc88effcbe87f0df7c7817
revoltek <rev@voo.it>
2018-11-05 16:05:55 +0000

    add

---
+++ msoverview2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import os, sys
+import casacore.tables as pt
+
+t = pt.table(sys.argv[1])
+times = sorted(set(t.getcol('TIME')))
+t.close()
+os.system('msoverview in=%s' % sys.argv[1])
+print "Time step %i seconds." % (times[1]-times[0])
