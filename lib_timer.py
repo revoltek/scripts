6ed580391dce59f4878ade041bb9882afe612777
Francesco Gasperin fdg@hs.uni-hamburg.de <baq1889@lgc2.lofar.gpu.cluster>
2018-12-18 00:42:35 +0100

    update

---
+++ lib_timer.py
@@ -0,0 +1,26 @@
+import time, logging
+
+class Timer(object):
+    """
+    context manager used to time the operations
+    """
+
+    def __init__(self, step = 'undefined', log = None):
+        """
+        log: is a logging istance to print the correct log format
+        if nothing is passed, root is used
+        """
+        if log is None: self.log = logging
+        else: self.log = log
+        self.step = step
+
+    def __enter__(self):
+        self.log.info("--> Starting \'" + self.step + "\'.")
+        self.start = time.time()
+        self.startcpu = time.clock()
+
+    def __exit__(self, exit_type, value, tb):
+
+        # if not an error
+        if exit_type is None:
+            self.log.debug("Time for this step: %i s (cpu: %i s)." % ( ( time.time() - self.start), (time.clock() - self.startcpu) ))
