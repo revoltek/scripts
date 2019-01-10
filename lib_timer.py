66dfbf73869d4da522b55894615cfe6a25863104
Francesco Gasperin fdg@hs.uni-hamburg.de <baq1889@node31.lofar.gpu.cluster>
2019-01-10 14:16:07 +0100

    update#

--- lib_timer.py
+++ lib_timer.py
@@ -23,4 +23,4 @@ class Timer(object):
 
         # if not an error
         if exit_type is None:
-            self.log.debug("<-- Time for this step: %i s (cpu: %i s)." % ( ( time.time() - self.start), (time.clock() - self.startcpu) ))
+            self.log.debug("<-- Time for %s step: %i s (cpu: %i s)." % ( self.step, ( time.time() - self.start), (time.clock() - self.startcpu) ))
