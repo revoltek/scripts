a82d248af3a202f5f6e932a2de4db3f081955dc9
Francesco Gasperin fdg@hs.uni-hamburg.de <baq1889@node31.lofar.gpu.cluster>
2019-04-26 18:16:44 +0200

    update

--- makepb.py
+++ makepb.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 
 import os, sys, glob
+import numpy as np
 from astropy.io import fits
 
 def make_beam(imageroot, outfile='beam.fits'):
 
-    imagefiles = sorted(glob.glob(imageroot+'0*-image.fits'))
-    beamfiles = sorted(glob.glob(imageroot+'0*-beam-I.fits'))
+    imagefiles = sorted(glob.glob(imageroot+'-0*-image.fits'))
+    beamfiles = sorted(glob.glob(imageroot+'-0*-beam-I.fits'))
+
+    if len(beamfiles) < 2 or len(imagefiles) < 2:
+        print('Need at least 2 beam images.')
+        sys.exit()
 
     # get weight
     weights = []
@@ -20,19 +25,24 @@ def make_beam(imageroot, outfile='beam.fits'):
         beam = beamfits[0].data * weights[0]
         header = beamfits[0].header
     for i, beamfile in enumerate(beamfiles[1:]):
-        with fits.open(beamfiles) as beamfits:
-            beam += beamfits[0].data * weigths[i+1]
+        with fits.open(beamfile) as beamfits:
+            beam += beamfits[0].data * weights[i+1]
 
     beam /= np.sum(weights)
-    fits.writeto(outfile, beam, header)
+    fits.writeto(outfile, beam, header, overwrite=True)
 
 if __name__=='__main__':
     import optparse
     opt = optparse.OptionParser(usage='%prog [-v|-V] image-root-name \n Francesco de Gasperin', version='1.0')
     opt.add_option('-o', '--outfile', help='Filename for the combined beam fits (default=beam.fits)', default='beam.fits')
+    opt.add_option('-i', '--infile', help='Root name of the beam images (default=None)', default=None)
     (options, args) = opt.parse_args()
 
-
     outfile = options.outfile
+    infile = options.infile
 
-    make_beam(args, outfile)
+    if infile is None:
+        print('Missing filename.')
+        sys.exit()
+
+    make_beam(infile, outfile)
