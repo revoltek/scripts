4739b8cf178df44d8dfd56c82850091c57c3304a
Francesco Gasperin fdg@hs.uni-hamburg.de <baq1889@node31.lofar.gpu.cluster>
2020-10-09 10:34:00 +0200

    update

--- lib_fits.py
+++ lib_fits.py
@@ -17,7 +17,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-
 from astropy.wcs import WCS as pywcs
 from astropy.io import fits as pyfits
 import numpy as np
