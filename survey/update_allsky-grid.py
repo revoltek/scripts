f92a230e8ef51eb52d06f410030410b72b6d6be7
Francesco Gasperin fdg@hs.uni-hamburg.de <baq1889@lgc2.lofar.gpu.cluster>
2022-04-05 13:55:56 +0200

    update

--- survey/update_allsky-grid.py
+++ survey/update_allsky-grid.py
@@ -2,11 +2,16 @@
 # cehck LTA and write the cycle for which we have observations in the grid file
 
 import os, sys, re, pickle
+import numpy as np
 from awlofar.database.Context import context
 from awlofar.main.aweimports import CorrelatedDataProduct, \
     FileObject, \
     Observation
 from awlofar.toolbox.LtaStager import LtaStager, LtaStagerError
+from astropy.utils import iers
+iers.IERS_A_URL='https://maia.usno.navy.mil/ser7/finals2000A.all'
+iers_a = iers.IERS_A.open(iers.IERS_A_URL)
+iers.earth_orientation_table.set(iers_a)
 from astropy.table import Table
 from astropy.coordinates import EarthLocation
 from astropy.time import Time
@@ -45,18 +50,18 @@ if not os.path.exists("update_allsky-grid.pickle"):
                     lst = astrotime.sidereal_time('mean', lofar_location.lon)
                     if time.year == 2021 and ( (time.month==2 and time.day>=8) or (time.month>2 and time.month<8) or ( time.month==8 and time.day<=3) ):
                         print('Add BAD obs to the list: %s' % (field_id))
-                        obs_all.append([field_id,'bad',obs_id,'-'])
+                        obs_all.append([field_id,'bad',obs_id,0])
                     else: 
-                        print('Add obs to the list: %s' % (field_id))
+                        print('Add obs to the list: %s (LST: %f)' % (field_id, lst))
                         obs_all.append([field_id,project,obs_id,lst])
 
     # add manual things:
 
     # Commissioning obs on SPARSE 12/2020
     for obs in ['P182+35','P184+37','P184+40','P185+35','P187+37','P187+40','P188+35','P190+37','P191+40']:
-        obs_all.append([obs,'comm2020'])
-        obs_all.append([obs,'comm2020'])
-        obs_all.append([obs,'comm2020'])
+        obs_all.append([obs,'comm2020',-1,0])
+        obs_all.append([obs,'comm2020',-1,0])
+        obs_all.append([obs,'comm2020',-1,0])
 
     pickle.dump(obs_all, open( "update_allsky-grid.pickle", "wb" ))
 
@@ -68,22 +73,23 @@ else:
 grid = Table.read('allsky-grid.fits')
 grid['hrs'] = 0
 grid['cycle'] = ''
-grid['obsid'] = ''
-grid['LST'] = ''
+grid['obsid'] = 0
+grid['LST'] = None
 for obs in obs_all:
     obs[0] = obs[0].strip()
-    idx = (grid['name'] == obs[0].upper())
-    if sum(idx) == 0: 
+    try:
+        idx = np.where(grid['name'] == obs[0].upper())[0][0]
+    except:
         print('WARNING: missing %s in the grid' % obs)
         continue
     if not obs[1] == 'bad': grid['hrs'][idx] += 1
-    if grid['cycle'][idx].tolist()[0] == '':
-        grid['cycle'][idx] = obs[1]
-        grid['obsid'][idx] = obs[2]
-        grid['LST'][idx] = obs[3]
-    else:
-        grid['cycle'][idx] = grid['cycle'][idx].tolist()[0]+','+obs[1]
-        grid['obsid'][idx] = grid['obsid'][idx].tolist()[0]+','+obs[2]
-        grid['LST'][idx] = grid['LST'][idx].tolist()[0]+','+obs[3]
+    idxcell = list(grid['obsid'][idx]).index(0)
+    #print(obs,idxcell)
+    grid['cycle'][idx][idxcell] = obs[1]
+    grid['obsid'][idx][idxcell] = obs[2]
+    try:
+        grid['LST'][idx][idxcell] = obs[3].hour
+    except:
+        grid['LST'][idx][idxcell] = 0
 
 grid.write('allsky-grid.fits', overwrite=True)
