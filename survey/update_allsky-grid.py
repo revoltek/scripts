9084c2768b3ac915454e9b487d3ee0c9cc43907c
Francesco Gasperin fdg@hs.uni-hamburg.de <baq1889@node35.lofar.gpu.cluster>
2022-08-01 07:57:20 +0200

    update

--- survey/update_allsky-grid.py
+++ survey/update_allsky-grid.py
@@ -22,6 +22,10 @@ projects = survey_projects.split(',')
 
 lofar_location = EarthLocation(lat=52.90889*u.deg, lon=6.86889*u.deg, height=0*u.m) # LOFAR
 
+# obs ids that are know to have bad data
+bad_obs_ids = [834468,2002342,827912,2002321,828950,828670,828870,854892,844722,844956,2001946,843486,844592,847236,846084,846784,846154,844532,842870,843246,844946,856844,845380,842910,844712,842298,844886,844866,845410,842930,846774,843476,846074,844936,841418,842980,843346,842214,854322,844612,844652,844702,844876,841568,842900,843426,843466,841478,844926,844996,2002410,840096,2002459,2002211,854882,844642,844692,857598,843456,843554,836831,828860,841086,839796,841140,838890,836821,831968,833148,831958,831668,835435,828086,855786,856954,844732,844582,846794,844622] # LC16_004
+bad_obs_ids += [845086,846714,845732,847996,845686,846674,845056,845798,845186,791676,791726,786033,789424,801616,791966,792096,791796,792066,792746,792036,792226,792106,844552,801706,801746,845066,847438,802360,801736,846624,847478,845096,855268,845714,846644,845742,845650,848016,801716,801606,846634,801756,847448,801766,845106,853972,844542,845156,845610,803744,803894,845778,854372,845166,845116,847986,845046,845076,847458,846704,845788,845676,845126,845176,845696,846654,790736,854052,787165,855178,786983,790666,789214,786525,790896,787637,786615,787647,787737,784095] # LC14_002
+
 # The class of data to query
 cls = CorrelatedDataProduct
 re_cal = re.compile('.*3[c|C](196|295|380).*')
@@ -49,6 +53,9 @@ if not os.path.exists("update_allsky-grid.pickle"):
                     astrotime = Time(time, format='datetime', scale='utc')
                     lst = astrotime.sidereal_time('mean', lofar_location.lon)
                     if time.year == 2021 and ( (time.month==2 and time.day>=8) or (time.month>2 and time.month<8) or ( time.month==8 and time.day<=3) ):
+                        print('Add BUG obs to the list: %s' % (field_id))
+                        obs_all.append([field_id,'bug',obs_id,0])
+                    elif obs_id in bad_obs_ids:
                         print('Add BAD obs to the list: %s' % (field_id))
                         obs_all.append([field_id,'bad',obs_id,0])
                     else: 
@@ -82,7 +89,7 @@ for obs in obs_all:
     except:
         print('WARNING: missing %s in the grid' % obs)
         continue
-    if not obs[1] == 'bad': grid['hrs'][idx] += 1
+    if not obs[1] == 'bug' and not obs[1] == 'bad': grid['hrs'][idx] += 1
     idxcell = list(grid['obsid'][idx]).index(0)
     #print(obs,idxcell)
     grid['cycle'][idx][idxcell] = obs[1]
