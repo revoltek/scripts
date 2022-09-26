2eea03d2c694703be92961a5e60eee728a9bbf46
Francesco De Gasperin <fdg@scheduler.ira.inaf.it>
2022-09-26 15:34:15 +0200

    update# Committer: Francesco De Gasperin <fdg@scheduler.ira.inaf.it>

--- survey/run_survey_pleiadi.py
+++ survey/run_survey_pleiadi.py
@@ -8,7 +8,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 singularity_img = '/homes/fdg/storage/pill.simg'
-singularity_cmd = 'singularity run --pid --writable-tmpfs --containall --cleanenv -B/homes/fdg:/homes/fdg,/local/work/fdg:/local/work/fdg,/iranet/lofarfs2/lofar2/fdg:/iranet/lofarfs2/lofar2/fdg '+singularity_img
+singularity_cmd = 'singularity exec --cleanenv --pwd /local/work/fdg --env PYTHONPATH=\$PYTHONPATH:/homes/fdg/storage/LiLF/:/homes/fdg/storage/scripts/,PATH=\$PATH:/homes/fdg/storage/LiLF/scripts/ --pid --writable-tmpfs --containall -B/homes/fdg,/local/work/fdg,/iranet/lofarfs2/lofar2/fdg '+singularity_img
 
 dir_storage_cals = '/iranet/lofarfs2/lofar2/fdg/surveycals'
 dir_storage_tgts = '/iranet/lofarfs2/lofar2/fdg/surveytgts'
@@ -19,7 +19,7 @@ dir_run = "/homes/fdg/storage/run"
 os.chdir(dir_run)
 os.system('rm sbatch_files/*')
 
-class Schedule():
+class Scheduler():
 
     def __init__(self, name):
 
@@ -39,7 +39,7 @@ class Schedule():
             dir_orig = dir_storage_cals+'/download/mss/'+self.name
             dir_dest = dir_storage_cals+'/done/'+self.name
 
-            content = """#!/bin/bash
+            content = f"""#!/bin/bash
                          #SBATCH --nodes=1
                          ### number of hyperthreading threads
                          #SBATCH --ntasks-per-core=1
@@ -48,19 +48,19 @@ class Schedule():
                          ### number of openmp threads
                          #SBATCH --cpus-per-task=36
                          #SBATCH --time=5:00:00
-                         #SBATCH -o %s
+                         #SBATCH -o {self.file_sbatch}.log
                          rm -r /local/work/fdg/*
-                         mkdir /local/work/fdg/cal
-                         echo -e "[LOFAR_cal]\ndata_dir=%s\n" > /local/work/fdg/lilf.config
-                         singularity exec --cleanenv --pwd /local/work/fdg/cal --env PYTHONPATH=\$PYTHONPATH:/homes/fdg/storage/LiLF/:/homes/fdg/storage/scripts/,PATH=\$PATH:/homes/fdg/storage/LiLF/scripts/ --pid --writable-tmpfs --containall -B/homes/fdg,/local/work/fdg,/iranet/lofarfs2/lofar2/fdg %s /homes/fdg/storage/LiLF/pipelines/LOFAR_cal.py
-                         mv /local/work/fdg/cal/plots* /local/work/fdg/cal/cal*h5 /local/work/fdg/cal/*logger /local/work/fdg/cal/logs* %s
+                         mkdir -p /local/work/fdg/
+                         echo -e "[LOFAR_cal]\ndata_dir={dir_orig}\n" > /local/work/fdg/lilf.config
+                         {singularity_cmd} /homes/fdg/storage/LiLF/pipelines/LOFAR_cal.py
+                         mv /local/work/fdg/plots* /local/work/fdg/cal*h5 /local/work/fdg/*logger /local/work/fdg/logs* {dir_dest}
                          rm -r /local/work/fdg/*
-                         """ % (self.file_sbatch+".log", dir_orig, singularity_img, dir_dest)
+                         """
             content = ''.join(line.lstrip(' \t') for line in content.splitlines(True)) # remove spaces
 
         elif mode == 'pill':
 
-            content = """#!/bin/bash
+            content = f"""#!/bin/bash
                          #SBATCH --nodes=1
                          ### number of hyperthreading threads
                          #SBATCH --ntasks-per-core=1
@@ -69,11 +69,15 @@ class Schedule():
                          ### number of openmp threads
                          #SBATCH --cpus-per-task=36
                          #SBATCH --time=200:00:00
-                         #SBATCH -o %s
+                         #SBATCH -o {self.file_sbatch}.log
                          rm -r /local/work/fdg/*
-                         xxx
-                         rm -r /local/work/fdg/*
-                         """ % (self.file_sbatch+".log", dir_orig, singularity_img, dir_dest)
+                         mkdir -p /local/work/fdg/
+                         {singularity_cmd} /homes/fdg/storage/LiLF/pipelines/PiLL.py
+                         target=`cat target.txt | cut -d "," -f 0`
+                         mkdir -p {dir_storage_tgts}/$target
+                         mv /local/work/fdg/$target/mss-avg /local/work/fdg/$target/ddcal {dir_storage_tgts}/$target
+                         #rm -r /local/work/fdg/*
+                         """
             content = ''.join(line.lstrip(' \t') for line in content.splitlines(True)) # remove spaces
 
  
@@ -82,38 +86,38 @@ class Schedule():
 
 
 # cals
-all_cals = sorted(glob.glob(dir_storage_cals+'/download/mss/id*'))
-logging.info('Setting up %i jobs.' % len(all_cals))
-
-i=0
-for dir_orig in all_cals:
-    dir_dest = dir_orig.replace('download/mss','done')
-    if not os.path.exists(dir_dest):
-        os.makedirs(dir_dest)
-
-    # skip if already done
-    if len(glob.glob(dir_dest+'/*h5')) == 4:
-        continue
-
-    # skip if not present
-    if len(glob.glob(dir_orig+'/*MS')) == 0:
-        continue
-
-    c = Schedule(name=dir_orig.split('/')[-1])
-    c.prepare_sbatch('cal')
-    c.submit()
-
-    # separate initial calls so initial cp is diluted
-    if i < 24:
-        time.sleep(120)
-    i+=1
-
-# tgts
-#for i in range(10):
-#    c = Schedule(name=dir_orig.split('/')[-1])
-#    c.prepare_sbatch('pill')
+#all_cals = sorted(glob.glob(dir_storage_cals+'/download/mss/id*'))
+#logging.info('Setting up %i jobs.' % len(all_cals))
+#
+#i=0
+#for dir_orig in all_cals:
+#    dir_dest = dir_orig.replace('download/mss','done')
+#    if not os.path.exists(dir_dest):
+#        os.makedirs(dir_dest)
+#
+#    # skip if already done
+#    if len(glob.glob(dir_dest+'/*h5')) == 4:
+#        continue
+#
+#    # skip if not present
+#    if len(glob.glob(dir_orig+'/*MS')) == 0:
+#        continue
+#
+#    c = Scheduler(name=dir_orig.split('/')[-1])
+#    c.prepare_sbatch('cal')
 #    c.submit()
 #
 #    # separate initial calls so initial cp is diluted
 #    if i < 24:
-#        time.sleep(3600)
+#        time.sleep(120)
+#    i+=1
+
+# tgts
+for i in range(10):
+    c = Scheduler('pill')
+    c.prepare_sbatch('pill')
+    c.submit()
+
+    # separate initial calls so the stagings+downloads are diluted
+    if i < 24:
+        time.sleep(3600)
