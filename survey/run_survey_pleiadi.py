b1c6be74b5cf5e6302cd39fb98261064c91e2bfc
Francesco De Gasperin <fdg@lofar2.ira.inaf.it>
2022-10-19 10:46:51 +0200

    update

--- survey/run_survey_pleiadi.py
+++ survey/run_survey_pleiadi.py
@@ -28,6 +28,7 @@ class Scheduler():
 
         self.name = name
         self.file_sbatch = "sbatch_files/"+name+'_'+id_generator()
+        self.file_log = self.file_sbatch+'.log'
 
     def submit(self):
         logging.info('Scheduling: %s' % self.file_sbatch)
@@ -48,9 +49,9 @@ class Scheduler():
                          ### number of openmp threads
                          #SBATCH --cpus-per-task=36
                          #SBATCH --time=5:00:00
-                         #SBATCH -o {self.file_sbatch}.log
+                         #SBATCH -o {self.file_log}
                          rm -r /local/work/fdg/*
-                         mkdir -p /local/work/fdg/
+                         mkdir -p /local/work/fdg
                          echo -e "[LOFAR_cal]\ndata_dir={dir_orig}\n" > /local/work/fdg/lilf.config
                          {singularity_cmd} /homes/fdg/storage/LiLF/pipelines/LOFAR_cal.py
                          mv /local/work/fdg/plots* /local/work/fdg/cal*h5 /local/work/fdg/*logger /local/work/fdg/logs* {dir_dest}
@@ -69,14 +70,10 @@ class Scheduler():
                          ### number of openmp threads
                          #SBATCH --cpus-per-task=36
                          #SBATCH --time=200:00:00
-                         #SBATCH -o {self.file_sbatch}.log
+                         #SBATCH -o {self.file_log}
                          rm -r /local/work/fdg/*
                          mkdir -p /local/work/fdg/
                          {singularity_cmd} /homes/fdg/storage/LiLF/pipelines/PiLL.py
-                         tar zcf mss.tgz mss-avg
-                         target=`cat target.txt | cut -d "," -f 0`
-                         mkdir -p {dir_storage_tgts}/$target
-                         mv /local/work/fdg/$target/mss.tgz {dir_storage_tgts}/$target
                          #rm -r /local/work/fdg/*
                          """
             content = ''.join(line.lstrip(' \t') for line in content.splitlines(True)) # remove spaces
@@ -87,38 +84,40 @@ class Scheduler():
 
 
 # cals
-#all_cals = sorted(glob.glob(dir_storage_cals+'/download/mss/id*'))
-#logging.info('Setting up %i jobs.' % len(all_cals))
-#
-#i=0
-#for dir_orig in all_cals:
-#    dir_dest = dir_orig.replace('download/mss','done')
-#    if not os.path.exists(dir_dest):
-#        os.makedirs(dir_dest)
-#
-#    # skip if already done
-#    if len(glob.glob(dir_dest+'/*h5')) == 4:
-#        continue
-#
-#    # skip if not present
-#    if len(glob.glob(dir_orig+'/*MS')) == 0:
-#        continue
-#
-#    c = Scheduler(name=dir_orig.split('/')[-1])
-#    c.prepare_sbatch('cal')
-#    c.submit()
-#
-#    # separate initial calls so initial cp is diluted
-#    if i < 24:
-#        time.sleep(120)
-#    i+=1
+all_cals = sorted(glob.glob(dir_storage_cals+'/download/mss/id*'))
+logging.info('Setting up %i jobs.' % len(all_cals))
 
-# tgts
-for i in range(10):
-    c = Scheduler('pill')
-    c.prepare_sbatch('pill')
+i=0
+for dir_orig in all_cals:
+    dir_dest = dir_orig.replace('download/mss','done')
+    if not os.path.exists(dir_dest):
+        os.makedirs(dir_dest)
+
+    # skip if already done
+    if len(glob.glob(dir_dest+'/*h5')) == 4:
+        continue
+
+    # skip if not present
+    if len(glob.glob(dir_orig+'/*MS')) == 0:
+        continue
+
+    c = Scheduler(name=dir_orig.split('/')[-1])
+    c.prepare_sbatch('cal')
     c.submit()
 
-    # separate initial calls so the stagings+downloads are diluted
+    # separate initial calls so initial cp is diluted
     if i < 24:
-        time.sleep(3600)
+        time.sleep(120)
+    i+=1
+
+# tgts
+#i=0
+#for i in range(10):
+#    c = Scheduler('pill')
+#    c.prepare_sbatch('pill')
+#    c.submit()
+#
+#    # separate initial calls so the stagings+downloads are diluted
+#    if i < 24:
+#        time.sleep(3600)
+#    i+=1
